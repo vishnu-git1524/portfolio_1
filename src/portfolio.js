91500a22f5cf3e498d5f3966cf03bc8a43251baa
vishnu-git1524 <vishnuvenkat1524@gmail.com>
2024-10-20 23:30:55 +0530

    portfolio_1

---
+++ src/portfolio.js
@@ -0,0 +1,46 @@
+const header = {
+  homepage: '',
+  title: 'K.S.M BHARADWAJ',
+}
+
+const about = {
+  name: 'K.S.M BHARADWAJ',
+  role: 'Aspiring CSE Student',
+  description:
+    "I'm a second-year B.Tech student specializing in Computer Science Engineering at SRM IST Ramapuram (2023-2027), with a focus on smart systems and real-world applications. I have experience in various programming languages like HTML, CSS, JavaScript, C, C++, and Python. Passionate about technology, I aim to apply my skills in innovative projects and collaborate with like-minded individuals in the tech industry. I'm constantly learning and exploring new technologies to stay current with industry trends and best practices. My goal is to contribute to innovative web solutions that make a positive impact on users' experiences",
+  resume: 'https://example.com',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/k-bharadwaj-34126a312?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app',
+    github: 'https://github.com/BHARADWAJ989',
+  },
+}
+
+const projects = [
+  // You can add your projects here
+]
+
+const skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'C',
+  'C++',
+  'Python',
+]
+
+const education = [
+  {
+    institution: 'SRM IST Ramapuram',
+    degree: 'B.Tech in Computer Science Engineering CSE ',
+    startYear: '2023',
+    endYear: '2027',
+    description: 'Specializing in CSE with a focus on smart systems and real-world applications.',
+  },
+]
+
+const contact = {
+  email: 'bharathramrrr@gmail.com',
+  phone: '9441468488',
+}
+
+export { header, about, projects, skills, education, contact }
